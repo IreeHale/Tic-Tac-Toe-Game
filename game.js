3e2b5a1d9d9d2fa5fbc39f61d250d68fea02c6ff
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-01-07 03:05:27 -0500

    working on tic tac toe game project

--- game.js
+++ game.js
@@ -8,7 +8,23 @@ window.addEventListener('DOMContentLoaded', () => {
     let isGameActive = true;
 
     const PLAYER_WON = 'PLAYERX_WON';
-    const PLAYERO_WON = 'PLAYERO_WON';    
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+    
+    const winningCondition = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    tiles.forEach((tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
 
     resetButton.addEventListener('click', resetBoard);
 });
