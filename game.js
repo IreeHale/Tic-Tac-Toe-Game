bce82a90b0d2b22ee7162dc388f4886c837212b1
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-01-07 02:18:43 -0500

    adding js functionality for the game to work

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log("Hello there");
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currnetPlayer = 'X';
+    let isGameActive = true;
+
+    const PLAYER_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';    
+
+    resetButton.addEventListener('click', resetBoard);
+});
