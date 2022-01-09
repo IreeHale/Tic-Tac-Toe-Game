6275c618e7f9ac8de52ba9a0b293186757a632d8
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-01-09 14:39:43 -0500

    adding js file functionality

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
-console.log("Hello There");
+const gameboard = document.getElementById("gameboard");
+const boxes = Array.from(document.getElementByClassName("box"));
+const restart = document.getElementById("restart");
+const playText = document.getElementById("playText");
+const spaces = [null, null, null, null, null, null, null, null, null];
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = O_TEXT;
+
+const drawBoard = () => {
+    boxes.forEach((box, index) => {
+        let styleString = "";
+        if (index < 3) {
+            styleString += `border-bottom: 3px solid black;`;
+        }
+        if (index % 3 === 0) {
+            styleString += `border-right: 3px solid black;`;
+        }
+        if (index % 3 === 2) {
+            styleString += `border-left: 3px solid black`;
+        }
+        if (index > 5) {
+            styleString += `borde-top: 3px solid black`;
+        }
+        box.style = styleString;
+    })
+}
